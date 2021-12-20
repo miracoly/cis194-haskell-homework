7bf95f9783ddd54afdd333ea8dc229c42867a17c
Miracoly <68049792+miracoly@users.noreply.github.com>
2021-12-20 20:22:24 +0100

    refactor 'insert' to match all patterns

--- app/Week2/LogAnalysis.hs
+++ app/Week2/LogAnalysis.hs
@@ -4,11 +4,11 @@ import Data.String
 import Week2.Log
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert (Unknown _) tree = tree
 insert msg Leaf = Node Leaf msg Leaf
 insert msg@(LogMessage _ newTime _) (Node left tmsg@(LogMessage _ time _) right)
   | newTime < time = Node (insert msg left) tmsg right
   | otherwise = Node left tmsg (insert msg right)
+insert _ tree = tree
 
 parse :: String -> [LogMessage]
 parse s = parseMessage <$> lines s
