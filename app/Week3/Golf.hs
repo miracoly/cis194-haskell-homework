95b2b3c1b1bed19dc5b407b8de2cd9f76bd5d6d8
Miracoly <68049792+miracoly@users.noreply.github.com>
2021-12-21 19:28:05 +0100

    add 'takeEveryNth' function with tests

--- app/Week3/Golf.hs
+++ app/Week3/Golf.hs
@@ -1,4 +1,20 @@
 module Week3.Golf where
+import Data.Maybe
+import Data.List (last)
 
-skips :: [a] -> [[a]]
-skips _ = error "TODO"
+-- skips :: [a] -> [[a]]
+-- skips [] = []
+-- skips xs = [xs] ++ 
+
+
+takeEveryNth :: Int -> [a] -> [a]
+takeEveryNth _ [] = []
+takeEveryNth n xs
+    | n < 1 = []
+    | n > length xs = []
+    | otherwise = (xs !! (n - 1)) : takeEveryNth n (drop n xs)
+
+-- safeLast :: [a] -> [a]
+-- safeLast xs
+--     | isNothing (Safe.lastMay xs) = []
+--     | otherwise = fromJust $ Safe.lastMay xs
--- test/week3-spec.hs
+++ test/week3-spec.hs
@@ -8,9 +8,18 @@ import System.Exit (exitFailure)
 
 main = hspec $ do
   describe "Test skips" $ do
-    it "should return correct list" $ do
-      skips "ABCD" `shouldBe` ["ABCD", "BD", "C", "D"]
-      skips "hello!" `shouldBe` ["hello!", "el!", "l!", "l", "o", "!"]
-      skips [1] `shouldBe` [[1]]
-      skips [True,False] `shouldBe` [[True,False], [False]]
-      skips [] `shouldBe` []
+    -- it "should return correct list" $ do
+      -- skips "ABCD" `shouldBe` ["ABCD", "BD", "C", "D"]
+      -- skips "hello!" `shouldBe` ["hello!", "el!", "l!", "l", "o", "!"]
+      -- skips [1] `shouldBe` [[1]]
+      -- skips [True,False] `shouldBe` [[True,False], [False]]
+      -- skips "" `shouldBe` []
+      -- safeLast "ABCD" `shouldBe` "D"
+
+    it "should return every nth elem in a list" $ do
+      takeEveryNth 99 "" `shouldBe` []
+      takeEveryNth 0 "ABCD" `shouldBe` []
+      takeEveryNth 1 "ABCD" `shouldBe` "ABCD"
+      takeEveryNth 2 "ABCD" `shouldBe` "BD"
+      takeEveryNth 3 "ABCD" `shouldBe` "C"
+      takeEveryNth 3 "ABCDEFGH" `shouldBe` "CF"
