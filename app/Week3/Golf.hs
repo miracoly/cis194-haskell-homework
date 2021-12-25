a0702efc4ae696fed63c0be95a8d701820aa33a8
Miracoly <68049792+miracoly@users.noreply.github.com>
2021-12-26 00:25:17 +0100

    add helper functions for 'histogram' function

--- app/Week3/Golf.hs
+++ app/Week3/Golf.hs
@@ -1,6 +1,42 @@
 module Week3.Golf where
 import Data.Maybe
-import Data.List (last)
+import Data.List (last, group, sort)
+
+histogram :: [Int] -> String
+histogram [] = "==========\n0123456789\n"
+histogram ls = unlines $ lines ls ++ [histogram []]
+    where
+        lines :: [Int] -> [String]
+        lines = map drawLine . reverse . extractLines . groupNumbers
+
+
+drawLine :: [Int] -> String
+drawLine = drawAsterisk "          "
+    where
+        drawAsterisk :: String -> [Int] -> String
+        drawAsterisk str [] = str
+        drawAsterisk str (x:xs) =
+            let (ys, _:zs) = splitAt x str in
+                drawAsterisk (ys ++ "*" ++ zs) xs
+
+groupNumbers :: [Int] -> [[Int]]
+groupNumbers = group . sort
+
+extractLines :: [[Int]] -> [[Int]]
+extractLines [[]] = []
+extractLines ls = [x | (x:_) <- ls] : extractLines (dropFirst ls)
+
+dropFirst :: [[Int]] -> [[Int]]
+dropFirst [] = []
+dropFirst ([]:ys) = dropFirst ys
+dropFirst ((x:xs):ys) = xs : dropFirst ys
+
+
+-- [1,1,1,5]  -> [[1],[1],[1, 5]]
+-- [1,4,5,4,6,6,3,4,2,4,9] -> [[4],[4],[4,6],[1,2,3,4,5,6,9]]
+-- [1,2,3,4,4,4,4,5,6,6,9] -> [[1],[2],[3],[4,4,4,4],[5],[6,6],[9]]
+--     -> [1,2,3,4,5,6,9] ++ [[4,4,4],[6]]
+--            "            -> [1,1,1,4,1,2,0,0,1]
 
 localMaxima :: [Integer] -> [Integer]
 localMaxima (x:y:z:xs)
--- test/week3-spec.hs
+++ test/week3-spec.hs
@@ -7,28 +7,51 @@ import Control.Exception (evaluate)
 import System.Exit (exitFailure)
 
 main = hspec $ do
-  describe "histogram" $ do
-    it "should return correct histogram string" $ do
-      histogram [1,1,1,5] `shouldBe`
-        " *        \n" ++
-        " *        \n" ++ 
-        " *   *    \n" ++
-        "==========\n" ++
-        "0123456789"
-      histogram [1,4,5,4,6,6,3,4,2,4,9] `shouldBe`
-        "    *     \n" ++
-        "    *     \n" ++
-        "    * *   \n" ++
-        " ******  *\n" ++
-        "==========\n" ++
-        "0123456789"
-      histogram [3,5] `shouldBe`
-        "   * *    \n" ++
-        "==========\n" ++
-        "0123456789\n"
-      histogram [] `shouldBe`
-        "==========\n" ++
-        "0123456789\n"
+  describe "extractLines" $ do
+    -- it "should drop first elem of every list elemt in list1" $ do
+    --   dropFirst [[1,5], [1], [1]] `shouldBe` [[5]]
+
+    it "should extract lines from list of Int lists" $ do
+      extractLines [[1,1,1],[5]] `shouldBe` [[1,5], [1], [1]]
+      extractLines [[1],[2],[3],[4,4,4,4],[5],[6,6],[9]]
+        `shouldBe` [[1,2,3,4,5,6,9],[4,6],[4],[4]]
+      -- extractLines [[3],[5]] `shouldBe` [[3,5]]
+
+  describe "groupNumbers" $ do
+    it "should transform list correctly" $ do
+      groupNumbers [1,2,3,4,4,4,4,5,6,6,9]
+        `shouldBe` [[1],[2],[3],[4,4,4,4],[5],[6,6],[9]]
+      groupNumbers [1,1,1,5] `shouldBe` [[1,1,1], [5]]
+      groupNumbers [3,5] `shouldBe` [[3],[5]]
+  
+  -- describe "drawLine" $ do
+  --   it "should draw line of histogram out of list" $ do
+  --     drawLine [0,1,2,3,4,5,6,7,8,9] `shouldBe` "**********"
+  --     drawLine [0,2,4,6,8] `shouldBe` "* * * * * "
+  --     drawLine [2,6] `shouldBe` "  *   *   "
+
+  -- describe "histogram" $ do
+  --   it "should return correct histogram string" $ do
+  --     -- histogram [1,1,1,5] `shouldBe`
+  --     --   " *        \n" ++
+  --     --   " *        \n" ++ 
+  --     --   " *   *    \n" ++
+  --     --   "==========\n" ++
+  --     --   "0123456789\n"
+  --     -- histogram [1,4,5,4,6,6,3,4,2,4,9] `shouldBe`
+  --     --   "    *     \n" ++
+  --     --   "    *     \n" ++
+  --     --   "    * *   \n" ++
+  --     --   " ******  *\n" ++
+  --     --   "==========\n" ++
+  --     --   "0123456789\n"
+  --     -- histogram [3,5] `shouldBe`
+  --     --   "   * *    \n" ++
+  --     --   "==========\n" ++
+  --     --   "0123456789\n"
+  --     histogram [] `shouldBe`
+  --       "==========\n" ++
+  --       "0123456789\n"
 
   describe "localMaxima" $ do
     it "should return [] if input is []" $ do
