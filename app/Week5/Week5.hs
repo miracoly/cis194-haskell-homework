8919badb3abe124f287dcf38253d6895a64a6ce5
Miracoly <68049792+miracoly@users.noreply.github.com>
2022-01-30 13:33:27 +0100

    add parts of exercise 4

--- app/Week5/Week5.hs
+++ app/Week5/Week5.hs
@@ -2,15 +2,45 @@ module Week5.Week5 where
 
 import Week5.ExprT
 import Week5.Parser (parseExp)
+import GHC.IO.Handle.Types (Handle__)
 
 eval :: ExprT -> Integer
 eval (Lit x) = x
-eval (Add x y) = (eval x) + (eval y)
-eval (Mul x y) = (eval x) * (eval y)
+eval (Add x y) = eval x + eval y
+eval (Mul x y) = eval x * eval y
 
 evalStr :: String -> Maybe Integer
-evalStr = evalExpr . parseExp Lit Add Mul 
+evalStr = evalExpr . parseExp Lit Add Mul
     where
         evalExpr :: Maybe ExprT -> Maybe Integer
         evalExpr (Just expr) = Just (eval expr)
-        evalExpr Nothing = Nothing 
+        evalExpr Nothing = Nothing
+
+class Expr a where
+    lit :: Integer -> a
+    add :: a -> a -> a
+    mul :: a -> a -> a
+
+instance Expr ExprT where
+    lit = Lit 
+    mul = Mul
+    add = Add 
+
+reify :: ExprT -> ExprT
+reify = id
+
+instance Expr Integer where
+    lit = id
+    mul = (*)
+    add = (+)
+
+integify :: Integer -> Integer
+integify = id
+
+instance Expr Bool where
+    lit = (0<) 
+    mul = (&&)
+    add = (||)
+
+boolify :: Bool -> Bool
+boolify = id
--- test/week5-spec.hs
+++ test/week5-spec.hs
@@ -9,12 +9,26 @@ import System.Exit (exitFailure)
 
 main = hspec $ do
   describe "exercise 1" $ do
-    it "should return correct result of eval'" $ do
+    it "should return correct result of eval" $ do
       eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) `shouldBe` 20
       eval (Add (Mul (Lit 2) (Lit 3)) (Lit 4)) `shouldBe` 10
 
   describe "exercise 2" $ do
-    it "should return correct result of evalStr'" $ do
+    it "should return correct result of evalStr" $ do
        evalStr "(2+3)*4" `shouldBe` Just 20
        evalStr "2+3*4" `shouldBe` Just 14
        evalStr "2+3*" `shouldBe` Nothing
+
+  describe "exercise 4" $ do
+    it "lit, mul & add should work for instance Integer" $ do
+      integify (lit 2) `shouldBe` 2
+      integify (add 2 3) `shouldBe` 5
+      integify (mul 2 3) `shouldBe` 6
+    it "lit, mul & add should work for instance Bool" $ do
+      boolify (lit 2) `shouldBe` True
+      boolify (lit (-3)) `shouldBe` False
+      boolify (mul (lit (-3)) (lit 2)) `shouldBe` False
+      boolify (mul (lit 3) (lit 2)) `shouldBe` True
+      boolify (add (lit (-3)) (lit 2)) `shouldBe` True
+      boolify (add (lit 3) (lit 2)) `shouldBe` True
+      boolify (add (lit (-3)) (lit (-2))) `shouldBe` False
