9fd4aa8f967838f00bb703774a028e2f53f67403
Miracoly <68049792+miracoly@users.noreply.github.com>
2022-02-10 17:40:54 +0100

    add exercise1 for week 6

--- app/Week6/Week6.hs
+++ app/Week6/Week6.hs
@@ -1,1 +1,14 @@
 module Week6.Week6 where
+
+fib :: Integer -> Integer
+fib n
+    | n <= 0 = 0
+    | n == 1 = 1
+    | otherwise = fib (n-1) + fib (n-2)
+
+fib1 :: [Integer]
+fib1 = map fib [0..]
+
+-- Solution by https://github.com/bschwb/cis194-solutions/blob/main/06-lazy/Fibonacci.hs
+fibs2 :: [Integer]
+fibs2 = let fibo a b = a : fibo b (a+b) in fibo 0 1
